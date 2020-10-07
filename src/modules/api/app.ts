a73e77f10f8d9658fb13e9799c7e6a91c77b788b
Guilherme Brito <guilhermebritto.prof@gmail.com>
2020-10-07 13:37:22 -0300

    Added App Error Class

--- src/modules/api/app.ts
+++ src/modules/api/app.ts
@@ -1,12 +1,34 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 import { createConnection } from 'typeorm';
 import routes from './routes';
+import cors from 'cors';
+import AppError from '../../errors/AppError';
 
 createConnection();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        status: 'error',
+        message: err.message
+      });
+    }
+
+    console.error(err);
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal Server Error'
+    });
+  }
+);
 
 export { app };
